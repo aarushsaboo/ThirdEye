89276469f2ca64f1162bcfb748651165250be522
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2024-11-04 15:00:51 +0530

    initial base commit

---
+++ she-builds-ai/src/App.js
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className={styles.navbar}>
+          
+      </div>
+    </div>
+  );
+}
+
+export default App;
