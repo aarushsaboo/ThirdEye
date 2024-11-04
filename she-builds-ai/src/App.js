3bc86383815d4208818c920b7d17d759f1103612
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2024-11-04 15:22:53 +0530

    2nd initial commit

--- she-builds-ai/src/App.js
+++ she-builds-ai/src/App.js
@@ -1,13 +1,18 @@
-import styles from './App.module.css';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <div className={styles.navbar}>
-          
-      </div>
-    </div>
-  );
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/landing" element={<LandingPage />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
